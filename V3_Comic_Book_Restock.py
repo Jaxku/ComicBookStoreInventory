59eda59873cc5977b59750918d81163c42f492dc
Jaxku <52820208+Jaxku@users.noreply.github.com>
2024-04-11 15:48:55 +1200

    help

--- V3_Comic_Book_Restock.py
+++ V3_Comic_Book_Restock.py
@@ -4,22 +4,29 @@ Restock comic books in the comic book store.
 
 
 class ComicBookStore:
-    def __init__(self):
-        self.comics = {
-            "Super Dude": 8,
-            "Lizard Man": 12,
-            "Water Woman": 3
-        }
-        self.total_sold = 0
+    comics = {
+        "Super Dude": 8,
+        "Lizard Man": 12,
+        "Water Woman": 3
+    }
+    total_sold = 0
 
     def sell_comic(self):
-        while True:
+        global comic_name
+        good_input = False
+        while not good_input:
             comic_name = input("Enter the name of the comic"
                                " you want to sell or type 'Quit': ")\
-                .title()  # Added .title() to make the input case-insensitive
+                .title()
+            comic = ComicBookStore.comics.get(comic_name)
 
             if comic_name == 'Quit':
                 break
+
+            if comic is None:
+                print(f"Sorry, {comic_name} does not exist. Please try again.")
+            else:
+                good_input = True
             if comic_name in self.comics and self.comics[comic_name] > 0:
                 self.comics[comic_name] -= 1
                 self.total_sold += 1
